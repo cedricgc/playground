4eaefd4240eaa44558100fa3efced015fb54b8c9
Cedricgc <cedric.charly@utexas.edu>
2014-05-27 13:03:28 -0500

    First 3 Euler Project questions

---
+++ Project Euler/euler1.js
@@ -0,0 +1,9 @@
+var total = 0;
+
+for (var i = 0; i < 1000; i++) {
+	if(i % 3 === 0 || i % 5 === 0) {
+		total += i;
+	}
+}
+
+console.log(total);
---
+++ Project Euler/euler3.js
@@ -0,0 +1,23 @@
+// The prime factors of 13195 are 5, 7, 13 and 29.
+
+// What is the largest prime factor of the number 600851475143 ?
+
+
+// Check out http://en.wikipedia.org/wiki/Primality_test
+// 
+
+var primes = [],
+	daNumber = 600851475143,
+	divider = 3;
+
+while(divider <= daNumber) {
+	while(daNumber % divider === 0) {
+		daNumber /= divider;
+		primes.push(divider);
+	}
+	// Skip even numbers
+	divider += 2
+}
+
+// The last element in the array is the largest prime factor
+console.log(primes[primes.length - 1]);
