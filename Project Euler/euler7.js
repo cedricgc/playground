47af7aa53419b03352d1d448e6b10457abfba7a2
Cedricgc <cedric.charly@utexas.edu>
2014-05-28 01:38:24 -0500

    Improved Euler 7, Euler 8

--- Project Euler/euler7.js
+++ Project Euler/euler7.js
@@ -4,10 +4,27 @@ What is the 10 001st prime number?*/
 
 // TODO Sieve Implementation
 
-First implementation
+// Improved isPrime function
+function isPrime2(n) {
+	if(n === 1) {return false;}
+	else if(n < 4) {return true;}
+	else if(n % 2 === 0) {return false;}
+	else if(n < 9) {return true;}
+	else if(n % 3 === 0) {return false;}
+	else {
+		var root = Math.floor(Math.sqrt(n));
+		for(i = 5; i <= root; i += 6) {
+			if(n % i === 0) { return false;}
+			else if(n % (i + 2) === 0) {return false;}
+		}
+		return true;
+	}
+}
+
+// First implementation
 function isPrime(n) {
 	if(n < 2) {return false;}
-	var root = Math.floor(Math.sqrt(n))
+	var root = Math.floor(Math.sqrt(n));
 	for(i = 2; i <= root; i++) {
 		if(n % i === 0) { return false;}
 	}
@@ -18,7 +35,7 @@ function nthPrime(place) {
 	var counter = 1;
 	var current = 3;
 	while(counter < place) {
-		if(isPrime(current)) {
+		if(isPrime2(current)) {
 			counter++;
 		}
 		// Skip even numbers
