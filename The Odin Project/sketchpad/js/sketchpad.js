9178bec6ffd64a8c24366e4578605f51d0249c61
Cedricgc <cedric.charly@utexas.edu>
2014-06-01 16:46:56 -0500

    reworked js, take in prompt

--- The Odin Project/sketchpad/js/sketchpad.js
+++ The Odin Project/sketchpad/js/sketchpad.js
@@ -1,79 +1,22 @@
-var DEFAULT_BACKGROUND = "#000000";
-var DEFAULT_COLOR = "#FFFFFF";
-var DEFAULT_SIDE = 10;
-var padWidth = $(".sketchpad").width();
-var padHeight = $(".sketchpad").height();
-var state = 1;
-var numberOfSquares;
-var currentColor;
+var defaultBoxes = 10;
 
-$(document).ready(function() {
-	setDefault();
-	// $(".square").mouseenter(function() {
-	// 	$(this).css("background-color", currentColor);
-	// });
-	$("#dimensions").click(function() {
-		var newSide = prompt("How many squares per side? return is default.").parseInt();
-		if ( isNaN(newSide)  || newSide < 1) {
-			newSide = 10;
-		}
-		setDimensions(newSide);
-	});
-	$("#color").click(function() {
-		clear();
-		state = 1;
-	});
-	$("#trail").click(function() {
-		clear();
-		state = 2;
-		console.log(state);
-	})
-	$("#random").click(function() {
-		clear();
-		state = 3;
-		console.log(state);
-	})
-	$(".square").mouseenter(function() {
-		if(state === 1) {
-			$(this).css("background-color", currentColor);
-		} else if(state === 2) {
-			$(this).fadeTo(0,0);
-		} else if(state === 3) {
-			currentColor = randomColor();
-			$(this).css("background-color", currentColor);
-		}
-	})
-});
+var generatePadType = function(option){
+	$('.sketchpad').html("");
 
-function setDefault() {
-	// calculate the length of each side of the square and
-	// the number of squares to add
-	setDimensions(DEFAULT_SIDE);
-	currentColor = DEFAULT_COLOR;
-}
-
-function clear() {
-	$(".square").css("background-color", DEFAULT_BACKGROUND);
-}
-
-function setDimensions(side) {
-	var squareSide = padWidth / side;
-	numberOfSquares = (padWidth * padHeight) / (squareSide * squareSide);
-	addSquares(squareSide, numberOfSquares);
-	clear();
-}
-
-function addSquares(side, numberOfSquares) {
-	$(".square").remove()
-	for(var i = 0; i < numberOfSquares; i++) {
-		$(".sketchpad").append("<div class=\"square\"></div>");
+	var boxesPerSide = parseInt(prompt("Choose a number between 1 and 128? " 
+		+ "RETURN for default."));
+	if(isNaN(boxesPerSide) || boxesPerSide < 1 || boxesPerSide > 128) {
+		boxesPerSide = defaultBoxes;
 	}
-	$(".square").height(side).width(side);
-}
-
-function randomColor() {
-	var red = Math.floor(Math.random() * 256)
-	var blue = Math.floor(Math.random() * 256)
-	var green = Math.floor(Math.random() * 256)
-	return "#" + red.toHex() + blue.toHex() + green.toHex();
+	console.log(boxesPerSide);
+	// At this point boxesPerSide is guaranteed to be an integer
+	var squareSide = $(".sketchpad").width() / boxesPerSide;
+	// Insert square class and break into sketchpad and set dimensions
+	for(var i = 0; i < boxesPerSide; i++) {
+		for(var j = 0; j < boxesPerSide; j++) {
+			$(".sketchpad").append('<div class="square"></div>');
+		}
+		$(".sketchpad").append('<div class="break"></div>');
+	}
+	$(".square").width(boxesPerSide).height(boxesPerSide);
 }
