b6218de0c4d4dfa97aebade6ebf6a999634afb53
Cedricgc <cedric.charly@utexas.edu>
2014-06-01 21:47:55 -0500

    reworked js, create grid

--- The Odin Project/sketchpad/js/sketchpad.js
+++ The Odin Project/sketchpad/js/sketchpad.js
@@ -1,22 +1,82 @@
-var defaultBoxes = 10;
+var defaultSquares = 25;
+var boxSize = $(".sketchpad").width();
 
-var generatePadType = function(option){
-	$('.sketchpad').html("");
+$(document).ready(function() {
+	createSketchPad(defaultSquares);
+	draw();
+	clear();
+	trail();
+	random();
+});
 
-	var boxesPerSide = parseInt(prompt("Choose a number between 1 and 128? " 
-		+ "RETURN for default."));
-	if(isNaN(boxesPerSide) || boxesPerSide < 1 || boxesPerSide > 128) {
-		boxesPerSide = defaultBoxes;
+function createSketchPad(userBoxes) {
+	var squareSize = boxSize / userBoxes;
+	for(var i = 0; i < (userBoxes * userBoxes); i++) {
+		$('.sketchpad').append('<div class="square"></div>');
 	}
-	console.log(boxesPerSide);
-	// At this point boxesPerSide is guaranteed to be an integer
-	var squareSide = $(".sketchpad").width() / boxesPerSide;
-	// Insert square class and break into sketchpad and set dimensions
-	for(var i = 0; i < boxesPerSide; i++) {
-		for(var j = 0; j < boxesPerSide; j++) {
-			$(".sketchpad").append('<div class="square"></div>');
+	$(".square").width(squareSize);
+	$(".square").height(squareSize);
+}
+
+function draw() {
+	$(".square").mouseover(function() {
+		$(this).css("background-color", "blue");
+	});
+}
+
+// wrapper for the button option
+function clear() {
+	$("#clear").click(function() {
+		clearBoard();
+		draw();
+	});
+}
+
+function clearBoard() {
+	$(".square").unbind(); // unbind to remove other event listeners on squares
+	$(".square").css("background-color", "black");
+	$(".square").css("opacity", 1);
+}
+
+// function outline() {
+// 	$("#borders").change(function() {
+// 		$(".square").addClass(".outline");
+// 	}));
+// }
+
+function newSize() {
+	$("#newSize").click(function() {
+		$(".sketchpad").html("");
+		var numSquares = prompt("How many boxes (1-128) do you want on each side? " + 
+			"Return for default sides.");
+		if(isNaN(numSquares) || numSquares < 1 || numSquares > 128) {
+			numSquares = defaultSquares;
 		}
-		$(".sketchpad").append('<div class="break"></div>');
-	}
-	$(".square").width(boxesPerSide).height(boxesPerSide);
+		createSketchPad(numSquares);
+	});
+}
+
+function random() {
+	$("#random").click(function() {
+		clearBoard();
+		$(".square").mouseover(function() {
+			$(this).css("background-color", getRandomColor());
+		});
+	});
+}
+
+function getRandomColor() {
+	return (Math.random().toString(16) + '000000').slice(2, 8);
+}
+
+function trail() {
+	$("#trail").click(function() {
+		clearBoard();
+		$(".square").mouseenter(function() {
+			$(this).fadeTo(0, 0);
+		});
+		$(".square").mouseleave(function() {
+			$(this).fadeTo(1000, 1);
+		});
+	});
 }
